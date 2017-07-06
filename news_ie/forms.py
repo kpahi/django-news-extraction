df7e7f9947ef77d8e9f421dbb4464ef1a33fd1e1
Basanta78 <basmhzn78@gmail.com>
2017-07-06 09:58:59 +0545

    UI

--- news_ie/forms.py
+++ news_ie/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class NameForm(forms.Form):
     #news = forms.CharField(label='detail news', max_length=500)
-    news_text = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 100}))
+    news_text = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 50}))
